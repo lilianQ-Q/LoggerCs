291779dedf8fb6795b164b21a09e3ba3be6f965f
Lilianux <lilianpierredamiens@gmail.com>
2020-11-18 15:38:06 +0100

    Ajout de l'interface Module

---
+++ src/modules/IModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noxLogger.src.modules
+{
+    public interface IModule
+    {
+        /// <summary>
+        /// Method that allow the user to get a Module's author.
+        /// </summary>
+        /// <returns></returns>
+        string GetAuthor();
+    }
+}
