88926ce96e51ddfd88d5a96c7b57676b905f03d6
Lilianux <lilianpierredamiens@gmail.com>
2020-11-19 16:20:10 +0100

    Ajout du module de sql logging

---
+++ src/modules/sqlLogger/SqlLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noxLogger.src
+{
+    public partial class Logger
+    {
+        /// <summary>
+        /// Method that allow the user to log a sql query. This one is parsed before insert.
+        /// </summary>
+        /// <param name="query"></param>
+        public void LogSQL(string query)
+        {
+            //Don't forget to parse the sql
+            this.Log(LogType.info, query);
+        }
+    }
+}
---
+++ src/modules/sqlLogger/src/core/SqlLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noxLogger.src.modules.sqlLogger.src.core
+{
+    public class SqlLogger : IModule
+    {
+        public SqlLogger()
+        {
+
+        }
+
+        public string GetAuthor()
+        {
+            return ("Lilian Damiens - 2020");
+        }
+    }
+}
