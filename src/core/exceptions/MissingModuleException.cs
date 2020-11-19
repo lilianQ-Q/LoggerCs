69ff34ff79590ddca9a9fb6bc59854296ce3c0cc
Lilianux <lilianpierredamiens@gmail.com>
2020-11-19 16:15:59 +0100

    Ajout de l'exception pour les modules manquants

---
+++ src/core/exceptions/MissingModuleException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noxLogger.src.core.exceptions
+{
+    /// <summary>
+    /// Throw this exception when a module is missing.
+    /// </summary>
+    public class MissingModuleException : Exception
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Main constructor of MissingModuleException.
+        /// </summary>
+        /// <param name="modulName"></param>
+        public MissingModuleException(string modulName) : base("Missing module inside projet : " + modulName + ".")
+        {
+
+        }
+
+        #endregion
+    }
+}
