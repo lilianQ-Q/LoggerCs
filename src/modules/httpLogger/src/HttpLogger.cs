892f243406f89e6fb1656ec55ae4e772c79c70b3
Lilianux <lilianpierredamiens@gmail.com>
2020-11-20 17:27:18 +0100

    Implémentation de nouvelles méthodes

--- src/modules/httpLogger/src/HttpLogger.cs
+++ src/modules/httpLogger/src/HttpLogger.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using noxLogger.src.core.entities;
+using noxLogger.src.core.exceptions;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace noxLogger.src.modules.httpLogger.src
@@ -12,6 +15,11 @@ namespace noxLogger.src.modules.httpLogger.src
         /// This is the token of the httpLogger.
         /// </summary>
         private string token { get; set; }
+        /// <summary>
+        /// Url of the api you want to insert data in. 
+        /// TODO : Make a list of apiUrl for multi insert
+        /// </summary>
+        private string apiUrl { get; set; }
 
         private string lastname = "Damiens";
         private string firstname = "Lilian";
@@ -25,8 +33,9 @@ namespace noxLogger.src.modules.httpLogger.src
         /// Main constructor of the HttpLogger class.
         /// </summary>
         /// <param name="token"></param>
-        public HttpLogger(string token)
+        public HttpLogger(string token, string apiUrl = "https://liliandamiens.fr/")
         {
+            this.apiUrl = apiUrl;
             this.token = token;
         }
 
@@ -40,7 +49,48 @@ namespace noxLogger.src.modules.httpLogger.src
         /// <returns></returns>
         public string GetAuthor()
         {
-            return ("Lilian Damiens - 2020");
+            return (this.firstname + " " + this.lastname + " " + this.date);
+        }
+
+        public bool PostLog(string logData)
+        {
+            bool isOk = true;
+            if (this.urlReachable())
+            {
+                //Do some stuff here.
+            }
+            else
+            {
+                if (Logger.enabledException)
+                {
+                    throw new IncorrectUrlException("Host unreachable. Please check that this one is up or if the url is valid");
+                }
+                isOk = false;
+            }
+            return (isOk);
+        }
+
+        public bool PostLog(Log log)
+        {
+            return (new Log());
+        }
+
+        private bool urlReachable()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(this.apiUrl);
+            request.Timeout = 2000;
+            request.Method = "HEAD"; // As per Lasse's comment
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (WebException)
+            {
+                return false;
+            }
         }
 
         #endregion
