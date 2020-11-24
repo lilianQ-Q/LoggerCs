f98dc0f18d7cb067ef2cacbd69346f5d08009efa
Lilianux <lilianpierredamiens@gmail.com>
2020-11-24 16:18:24 +0100

    Renommage de l'exception pour les fichiers non trouvés

---
+++ src/core/exceptions/FileNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace noxLogger.src.core.exceptions
+{
+    /// <summary>
+    /// Custom exception casted when specified file does not exists.
+    /// </summary>
+    public class FileNotFoundException : Exception
+    {
+        /// <summary>
+        /// Main constructor of FileDoesntExists class.
+        /// </summary>
+        /// <param name="path"></param>
+        public FileNotFoundException(string path) : base ("Error. The file at '" + path + "' doesn't exists. Please check the path and try again.")
+        {
+
+        }
+    }
+}
